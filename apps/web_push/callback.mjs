48dd9e6c8ba4a95f9fa140aa3fde61eb99500515
luomengling <luomengling@gysk.com.cn>
2025-07-07 17:04:19 +0800

    推送接口

--- apps/web_push/callback.mjs
+++ apps/web_push/callback.mjs
@@ -5,13 +5,13 @@ const callback = async (dataObject, ...otherArgs) => {
     globalThis._config.saveTweets()
     // --- or ---
     // post to another endpoint...
-    // await fetch('your-endpoint', {
-    //     method: 'POST',
-    //     headers: {
-    //         'Content-Type': 'application/json'
-    //     },
-    //     body: JSON.stringify(dataObject)
-    // })
+    await fetch('http://57.181.43.126:8001/api/receive/twitter/websocket', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(dataObject)
+    })
     // --- or ---
     // fetch more info from twitter...
     // const tweetInfo = await (await fetch('https://cdn.syndication.twimg.com/tweet-result?id=' + dataObject.tag.replace(/[^\d]+\-/gm, '') + '&token=0')).json()
